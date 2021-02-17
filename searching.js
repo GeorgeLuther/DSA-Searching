d44daaaaf563f69a36afeb60b6da32d0b4208e85
George Luther <george_l@thinkful.com>
2021-02-17 15:03:00 -0500

    added sample data

--- searching.js
+++ searching.js
@@ -22,4 +22,40 @@ function binarySearch(arr, val, start, end){
     }
 }
 
-// 1. 
+/* 1.  
+    - 11, 6, 8
+    - 11, 15, 17 ... -1    
+*/
+
+/* 3.
+    - category
+    - isFiction
+- true > call number:
+    - section
+    - first 3 letters last name
+- false > call number:
+    - section
+    - 
+
+    category is a BST ROW?
+    section is a parent 
+
+    
+*/
+const library = [
+    { author: 'Cowlishaw, Mike', dewey: '005.133', title: 'The REXX Language' },
+    { author: 'Sams', dewey: '005.133', title: 'Teach Yourself C++ In 21 Days' },
+    { author: 'Stroustrup., Bjarne', dewey: '005.133', title: 'The C++ Programming Language' },
+    { author: 'Crockford, Douglas', dewey: '005.2762', title: 'JavaScript: The Good Parts' },
+    { author: 'Flanagan, David', dewey: '005.2762', title: 'JavaScript: The Definitive Guide' },
+    { author: 'Schmidt, Meinhard', dewey: '005.44684', title: 'Windows Vista for Dummies' },
+    { author: 'Zondervan', dewey: '220.52081', title: 'NIV Study Bible' },
+    { author:'Humphries, Russell, Dr.', dewey: '231.7652', title: 'Starlight and Time' },
+    { author: 'Jane, Frederick Thomas', dewey: '623.82509051', title: 'Jane\'s Fighting Ships' },
+    { author: 'Norris, Chuck', dewey: '796.8092', title: 'The Official Chuck Norris Fact Book' }
+  ];
+
+
+  /* 4. 
+  
+  */
